5c39d92fdb1008153f575080cf5f36709ad13e01
Pedro <pedrocascalheira25@gmail.com>
2023-03-23 18:09:53 +0000

    Layout

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let count = 0;
 function Menu(){
     const navStyle = document.querySelector('#nav')
 
+
     if(window.innerWidth <= 600){
         if(navStyle.style.display === 'none'){
             navStyle.style.display = 'block';
